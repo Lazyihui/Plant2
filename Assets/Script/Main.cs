743493eabf3faf6d904687fa930d4028cd86164c
包子 <chengbaozi6@qq.com>
2024-02-16 01:41:38 +0800

    prefab 和 得到canvas的方法

--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour {
+    //拖拽绑定
+    // [SerializeField] Canvas canvas;
+    Canvas canvas;
 
-    [SerializeField] Panel_Login panel_Login;
+    [SerializeField] Panel_Login panel_LoginPrefab;
 
 
     void Start() {
+
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        // canvans = GameObject.GetComponenetChirld<Canvas>();
+        //实例化 GameObject.Instantiate
+        Panel_Login panel_Login = GameObject.Instantiate(panel_LoginPrefab, canvas.transform);
 
         panel_Login.Ctor();
         panel_Login.Show();
