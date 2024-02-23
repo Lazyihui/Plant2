9f20c8a546e5fde57d61d30e1599e42f0c6ee8b9
包子 <chengbaozi6@qq.com>
2024-02-23 23:16:39 +0800

    <WIP> TM_Home

---
+++ Assets/Script/Template_Infrastructure/HomeTM.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TM_Home", menuName = "Template/TM_Home")]
+public class HomeTM : ScriptableObject {
+
+    public int TypeID;
+    public string typeName;
+
+    public Vector2 pos;
+
+
+}
